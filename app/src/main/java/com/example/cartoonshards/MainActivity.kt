0360c520a3a9e9f9b036d500534a489bd59b8a00
Maria <korchagina.adv@gmail.com>
2021-07-25 15:58:02 +0200

    Adding shards

--- app/src/main/java/com/example/cartoonshards/MainActivity.kt
+++ app/src/main/java/com/example/cartoonshards/MainActivity.kt
@@ -7,9 +7,12 @@ import com.example.cartoonshards.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //setListener()
     }
--- app/src/main/java/com/example/cartoonshards/gameFragments/GameFragment.kt
+++ app/src/main/java/com/example/cartoonshards/gameFragments/GameFragment.kt
@@ -1,6 +1,7 @@
 package com.example.cartoonshards.gameFragments
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,19 +11,62 @@ import com.example.cartoonshards.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
 
+    private val gameTime = 60000L
+    private val second = 1000L
     private lateinit var binding: FragmentGameBinding
+    private var questionIndex = 0
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    data class Question(val image: Int, val answers: List<String>)
+
+    private val questions: MutableList<Question> = mutableListOf(
+        Question(
+            image = R.drawable.tom_jerry,
+            answers = listOf("Tom & Jerry", "Finding Nemo", "Woody Woodpecker", "Lion King")
+        ),
+        Question(
+            image = R.drawable.stitch,
+            answers = listOf("Lilo & Stitch", "Jungle Book", "Bamby", "Duck Story")
+        )
+    )
+
+//    init {
+//        timer = object : CountDownTimer(gameTime, second) {
+//            override fun onTick(millisUntilFinished: Long) {
+//                currentTime = (millisUntilFinished / second).toString()
+//            }
+//
+//            override fun onFinish() {
+//                TODO("Not yet implemented")
+//            }
+//        }
+//    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        binding = FragmentGameBinding.inflate(layoutInflater, container, false)
+
+        binding = FragmentGameBinding.inflate(layoutInflater)
+        randomizeQuestions()
+        binding.imageView.setImageResource(questions[questionIndex].image)
+        setButtons()
+
         return binding.root
     }
 
+    private fun randomizeQuestions() {
+        questions.shuffle()
+        questionIndex = 0
+    }
+
+    private fun setButtons() {
+        val shuffledAnswers = questions[questionIndex].answers.toMutableList()
+        shuffledAnswers.shuffle()
+
+        with(binding) {
+            answer1.text = shuffledAnswers[0]
+            answer2.text = shuffledAnswers[1]
+            answer3.text = shuffledAnswers[2]
+            answer4.text = shuffledAnswers[3]
+        }
+    }
 }
