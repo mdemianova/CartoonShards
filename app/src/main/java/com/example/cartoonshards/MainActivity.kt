83e58daf8774ed1fae2d506f7fa76ee78c95597b
Maria <korchagina.adv@gmail.com>
2021-07-26 15:41:00 +0200

    Big screen done

--- app/src/main/java/com/example/cartoonshards/MainActivity.kt
+++ app/src/main/java/com/example/cartoonshards/MainActivity.kt
@@ -17,14 +17,6 @@ class MainActivity : AppCompatActivity() {
         //setListener()
     }
 
-//    private fun setListener() {
-//        val shards: List<View> = listOf(binding.view1, binding.view2, binding.view3, binding.view4,
-//            binding.view5, binding.view6, binding.view7, binding.view8, binding.view9)
-//
-//        for (item in shards) {
-//
-//            item.setOnClickListener { it.visibility = View.INVISIBLE }
-//        }
-//    }
+
 
 }
--- app/src/main/java/com/example/cartoonshards/gameFragments/GameFragment.kt
+++ app/src/main/java/com/example/cartoonshards/gameFragments/GameFragment.kt
@@ -1,20 +1,20 @@
 package com.example.cartoonshards.gameFragments
 
 import android.os.Bundle
-import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.example.cartoonshards.R
 import com.example.cartoonshards.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
 
-    private val gameTime = 60000L
-    private val second = 1000L
     private lateinit var binding: FragmentGameBinding
     private var questionIndex = 0
+    private lateinit var shards: List<View>
 
     data class Question(val image: Int, val answers: List<String>)
 
@@ -25,30 +25,28 @@ class GameFragment : Fragment() {
         ),
         Question(
             image = R.drawable.stitch,
-            answers = listOf("Lilo & Stitch", "Jungle Book", "Bamby", "Duck Story")
+            answers = listOf("Lilo & Stitch", "Jungle Book", "Bamby and 1000 Rabbits", "Duck Story")
         )
     )
-
-//    init {
-//        timer = object : CountDownTimer(gameTime, second) {
-//            override fun onTick(millisUntilFinished: Long) {
-//                currentTime = (millisUntilFinished / second).toString()
-//            }
-//
-//            override fun onFinish() {
-//                TODO("Not yet implemented")
-//            }
-//        }
-//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
 
         binding = FragmentGameBinding.inflate(layoutInflater)
+
+        shards = listOf(
+            binding.shard1, binding.shard2, binding.shard3, binding.shard4,
+            binding.shard5, binding.shard6, binding.shard7, binding.shard8,
+            binding.shard9, binding.shard10, binding.shard11, binding.shard12,
+            binding.shard12, binding.shard13, binding.shard14, binding.shard15,
+            binding.shard16
+        )
+
         randomizeQuestions()
-        binding.imageView.setImageResource(questions[questionIndex].image)
-        setButtons()
+        setScreen()
+        setListener()
+        setAnswerListener()
 
         return binding.root
     }
@@ -58,7 +56,8 @@ class GameFragment : Fragment() {
         questionIndex = 0
     }
 
-    private fun setButtons() {
+    private fun setScreen() {
+        binding.imageView.setImageResource(questions[questionIndex].image)
         val shuffledAnswers = questions[questionIndex].answers.toMutableList()
         shuffledAnswers.shuffle()
 
@@ -67,6 +66,36 @@ class GameFragment : Fragment() {
             answer2.text = shuffledAnswers[1]
             answer3.text = shuffledAnswers[2]
             answer4.text = shuffledAnswers[3]
+        }
+    }
+
+    private fun setListener() {
+        for (item in shards) {
+            item.setOnClickListener { it.visibility = View.INVISIBLE }
+        }
+    }
+
+    private fun setAnswerListener() {
+        val answerButton: List<Button> = listOf(
+            binding.answer1, binding.answer2, binding.answer3, binding.answer4
+        )
+
+        for (item in answerButton) {
+            item.setOnClickListener {
+                if (item.text == questions[questionIndex].answers[0]) {
+                    questionIndex = (questionIndex + 1) % questions.size
+                    refreshShards()
+                    setScreen()
+                } else {
+                    Toast.makeText(this.context, "Wrong", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun refreshShards() {
+        for (item in shards) {
+            item.visibility = View.VISIBLE
         }
     }
 }
